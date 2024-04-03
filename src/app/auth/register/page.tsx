038da1457b65d292f6e274876a4892fb18eefe2f
Blong711 <baolong0711@gmail.com>
2024-04-03 13:54:46 +0700

    Long

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,27 +1,33 @@
 "use client";
 import { EmailAddress, ForgotPassword, Href, Password, RememberPassword, SignIn } from "@/Constant";
 import { useAppSelector } from "@/Redux/Hooks";
-import { Mail, Lock } from "react-feather";
+import { Mail, Lock, Phone } from "react-feather";
 import {  ChangeEvent, FormEvent, useState } from "react";
 import axios from 'axios'; 
 import { Button, Col, Container, Form, FormGroup, Input, InputGroup, InputGroupText, Label, Row } from "reactstrap";
 import { API_URL } from "@/Components/apiConfig";
 
 
-const UserLogin = () => {
+const UserRegister = () => {
   const { i18LangStatus } = useAppSelector((store) => store.langSlice);
   const [showPassWord, setShowPassWord] = useState(false);
   const [email, setEmail] = useState(''); 
   const [password, setPassword] = useState('');
-  const handleLogin = async () => {
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [numberPhone, setNumberPhone] = useState('');
+
+  const handleRegister = async () => {
       console.log('Email: '+email);
       console.log('Password: '+password);
       let dataPost: { [key: string]: any } = {};
       dataPost['email'] = email;
       dataPost['password'] = password;
+      dataPost['confirmPassword'] = confirmPassword;
+      dataPost['numberPhone'] = numberPhone;
+
       
       try {
-        let signLogin = await axios.post(`${API_URL}/login`, dataPost);
+        let signLogin = await axios.post(`${API_URL}/register`, dataPost);
         let dataResponse = await signLogin.data;
         const token = dataResponse.token;
         localStorage.setItem('token', token);
@@ -39,8 +45,7 @@ const UserLogin = () => {
           <div className="login-card1">
             <Form className="theme-form login-form">
               <div className="login-header ">
-                <h4>Sign In</h4>
-                <h6>Welcome back! Log in to your account.</h6>
+                <h4>Register</h4>             
               </div>
               <FormGroup>
                 <Label>{EmailAddress}</Label>
@@ -57,17 +62,30 @@ const UserLogin = () => {
                   <div className="show-hide"><span className={showPassWord ? "hide" : "show"} onClick={() => setShowPassWord(!showPassWord)}></span></div>
                 </InputGroup>
               </FormGroup>
+              <FormGroup>
+                <Label>Confirm Password</Label>
+                <InputGroup>
+                  <InputGroupText><Lock /></InputGroupText>
+                  <input type={showPassWord ? "text" : "password"} name="confirmPassword" placeholder="Typing Confirm Password ..." className="block px-3 py-2 rounded-lg border border-gray-300" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                  <div className="show-hide"><span className={showPassWord ? "hide" : "show"} onClick={() => setShowPassWord(!showPassWord)}></span></div>
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <Label>Phone Number</Label>
+                <InputGroup>
+                  <InputGroupText><Phone /></InputGroupText>
+                  <input type="numberPhone" name="numberPhone" placeholder="Typing Phone Number ..." className="block px-3 py-2 rounded-lg border border-gray-300" value={numberPhone} onChange={(e) => setNumberPhone(e.target.value)}/>
+                </InputGroup>
+              </FormGroup>
               <FormGroup className="login-btn">
                 <div className="checkbox">
                   <Input id="checkbox1" type="checkbox" />
-                  <Label className="text-muted" for="checkbox1">{RememberPassword}</Label>
                 </div>
-                <a href={Href} className="link">{ForgotPassword}?</a>
               </FormGroup>
               <FormGroup>
-              <Button type="button" className="mt-3 px-3 py-2 rounded-lg bg-blue-700 border text-white" onClick={handleLogin}>Login</Button>
+              <Button type="button" className="mt-3 px-3 py-2 rounded-lg bg-blue-700 border text-white" onClick={handleRegister}>Register</Button>
               </FormGroup>
-              <p>Don't have account ?<a className="ms-2" href="/register">Create Account</a></p>
+              <p>Already have account ?<a className="ms-2" href="auth/login">Sign in</a></p>
             </Form>
           </div>
         </Col>
@@ -76,4 +94,4 @@ const UserLogin = () => {
   );
 };
 
-export default UserLogin;
+export default UserRegister;
